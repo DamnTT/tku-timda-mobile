a4783a178d1fdd15d51f8a1334132a3c96533d1c
DamnTT <thomas19980311@gmail.com>
2021-12-21 00:23:00 +0800

    把rvizweb藉由docker打出來

--- docker/dev.sh
+++ docker/dev.sh
@@ -2,4 +2,15 @@
 WORK_ENV=/home/damn/timda-mobile/src
 USER=docker-damn
 #docker run -it -v ${WORK_ENV}:/home/${USER}/timda-mobile/src ros-env 
-docker run -it -v ${WORK_ENV}:/root/timda-mobile/src ros-env 
+#docker run -it -v ${WORK_ENV}:/root/timda-mobile/src ros-env 
+docker run -it \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -e DISPLAY=unix$DISPLAY \
+  -e GDK_SCALE \
+  -e GDK_DPI_SCALE \
+  -v ${WORK_ENV}:/root/timda-mobile/src \
+  --name test \
+  -p 8001:8001 \
+  -p 80:80 \
+  ros-env
