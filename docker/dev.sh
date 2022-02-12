bb71021db160fe9b560f5a9423e1c04b572943c8
DamnTT <thomas19980311@gmail.com>
2022-02-12 15:13:53 +0800

    修改docker的腳本，並且新增dev-db的虛擬硬碟

--- docker/dev.sh
+++ docker/dev.sh
@@ -4,13 +4,15 @@ USER=docker-damn
 #docker run -it -v ${WORK_ENV}:/home/${USER}/timda-mobile/src ros-env 
 #docker run -it -v ${WORK_ENV}:/root/timda-mobile/src ros-env 
 docker run -it \
+  --runtime=nvidia \
   -v /etc/localtime:/etc/localtime:ro \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e DISPLAY=unix$DISPLAY \
   -e GDK_SCALE \
   -e GDK_DPI_SCALE \
   -v ${WORK_ENV}:/root/timda-mobile/src \
-  --name test \
+  -v dev-db:/root/timda-mobile/ \
+  --name timda-mobile \
   -p 8001:8001 \
   -p 80:80 \
   ros-env
